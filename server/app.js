c5cd8dbc54b6281cb3ee8bacebc0a2ceeb4a04e6
ScottRudiger <scottrudiger@gmail.com>
2017-06-29 12:10:11 -0700

    Add seed data for single-user testing

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.use(cors({
 }));
 
 app.use(express.static(path.join(__dirname, '/../public')));
-app.use('/user', router);
+app.use('/', router.auth);
+app.use('/user', router.user);
 
 module.exports = app;
--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('../config.js');
+const bcrypt = require('bcrypt-nodejs');
+const Promise = require('bluebird');
 
 const Schema = mongoose.Schema;
 
@@ -11,6 +13,10 @@ const userSchema = new Schema({
     index: {
       unique: true,
     },
+  },
+  password: {
+    type: String,
+    required: true,
   },
   email: {
     type: String,
@@ -26,5 +32,36 @@ const userSchema = new Schema({
 });
 
 const User = mongoose.model('User', userSchema);
+
+User.comparePassword = function(candidatePassword, savedPassword, cb) {
+  bcrypt.compare(candidatePassword, savedPassword, function(err, isMatch) {
+    if (err) { return cb(err); }
+    cb(null, isMatch);
+  });
+};
+
+userSchema.pre('save', function(next) {
+  this.profilePic = 'http://www.lovemarks.com/wp-content/uploads/profile-avatars/default-avatar-tech-guy.png';
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  User.remove();
+
+  var testUser = {
+    firstName: 'John',
+    lastName: 'Doe',
+    username: 'john_doe',
+    password: 'john123',
+    email: 'john@doe.com',
+  };
+
+  User.create(testUser);
+};
 
 module.exports = User;
