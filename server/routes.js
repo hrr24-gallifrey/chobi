990666af581811256e6b696cebd69a84668d9b38
Kenneth Marshall <kdmarshall89@gmail.com>
2017-06-28 21:12:55 -0500

    add routes for getting and uploading pics

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,13 @@ var requestHandler = require('./lib/request-handler.js');
 
 router.get('/:id', requestHandler.getUser);
 
-router.post('/:id/:albumName/:photoUrl', requestHandler.createNewAlbumOnePhoto);
+router.get('/:id/albums', requestHandler.getAlbums);
+
+router.get('/:id/pics', requestHandler.getPics);
+
+router.post('/:id/upload', requestHandler.createNewAlbumOnePhoto);
+
+router.post('/:id/album/', requestHandler.createNewAlbum);
 
 router.post('/create', requestHandler.createUser);
 
